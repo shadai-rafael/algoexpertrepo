129166420c5a65016c365ad5b5988c175ea6f6e2
shadai-rafael <shadai.lopez.garcia@gmail.com>
2021-02-20 17:13:21 -0600

    Minmax stack algoexpert solution1
    Signed-off-by: shadai-rafael <shadai.lopez.garcia@gmail.com>

---
+++ stack/stack.go
@@ -0,0 +1,132 @@
+package stack
+
+/**********************************************
+*	Min Max Stack construction
+***********************************************/
+type MinMaxTag string
+
+const (
+	Min MinMaxTag = "min"
+	Max           = "max"
+)
+
+type Node struct {
+	Value int
+	Next  *Node
+}
+
+type MinMaxStack struct {
+	Top    *Node
+	MinMax map[MinMaxTag]*Node
+}
+
+func NewMinMaxStack() *MinMaxStack {
+	return &MinMaxStack{
+		Top:    nil,
+		MinMax: make(map[MinMaxTag]*Node),
+	}
+}
+
+func (stack *MinMaxStack) Peek() int {
+	if stack.isEmpty() {
+		return -1
+	}
+	return stack.Top.Value
+}
+
+func (stack *MinMaxStack) findMax() *Node {
+	maxNode := stack.Top
+	for node := stack.Top; node != nil; node = node.Next {
+		if maxNode.Value < node.Value {
+			maxNode = node
+		}
+	}
+	return maxNode
+}
+
+func (stack *MinMaxStack) findMin() *Node {
+	minNode := stack.Top
+	for node := stack.Top; node != nil; node = node.Next {
+		if minNode.Value > node.Value {
+			minNode = node
+		}
+	}
+	return minNode
+}
+
+func (stack *MinMaxStack) Pop() int {
+	if stack.isEmpty() {
+		return -1
+	}
+	removedNode := stack.Top
+	stack.Top = removedNode.Next
+	if stack.isEmpty() {
+		stack.MinMax[Max] = nil
+		stack.MinMax[Min] = nil
+	} else if stack.Top.Next == nil {
+		stack.MinMax[Max] = stack.Top
+		stack.MinMax[Min] = stack.Top
+	} else if stack.MinMax[Max] == removedNode {
+		stack.MinMax[Max] = stack.findMax()
+	} else if stack.MinMax[Min] == removedNode {
+		stack.MinMax[Min] = stack.findMin()
+	}
+	return removedNode.Value
+}
+
+func (stack *MinMaxStack) Push(number int) {
+	node := Node{
+		Value: number,
+		Next:  stack.Top,
+	}
+	if stack.isEmpty() {
+		stack.MinMax[Min] = &node
+		stack.MinMax[Max] = &node
+	} else {
+		if stack.MinMax[Min].Value > node.Value {
+			stack.MinMax[Min] = &node
+		} else if stack.MinMax[Max].Value < node.Value {
+			stack.MinMax[Max] = &node
+		}
+	}
+	stack.Top = &node
+}
+
+func (stack *MinMaxStack) GetMin() int {
+	if stack.isEmpty() {
+		return -1
+	}
+	return stack.MinMax[Min].Value
+}
+
+func (stack *MinMaxStack) GetMax() int {
+	if stack.isEmpty() {
+		return -1
+	}
+	return stack.MinMax[Max].Value
+}
+
+func (stack *MinMaxStack) isEmpty() bool {
+	if stack.Top == nil {
+		return true
+	}
+	return false
+}
+
+/**********************************************
+*	Balanced Brackets
+***********************************************/
+
+/*func BalancedBrackets(s string) bool {
+	// Write your code here.
+	return false
+}*/
+
+/**********************************************
+*	Sunset View
+***********************************************/
+
+/*func SunsetViews(buildings []int, direction string) []int {
+	// Write your code here.
+	return []int{}
+}*/
