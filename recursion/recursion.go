eee6debcb2834450591897a8151d702cd9a92111
shadai-rafael <shadai.lopez.garcia@gmail.com>
2021-03-06 11:55:34 -0600

    Product sum solution 1
    Signed-off-by: shadai-rafael <shadai.lopez.garcia@gmail.com>

--- recursion/recursion.go
+++ recursion/recursion.go
@@ -11,3 +11,26 @@ func GetNthFib(n int) int {
 	}
 	return GetNthFib(n-1) + GetNthFib(n-2)
 }
+
+/**********************************************
+*	Product Sum
+***********************************************/
+type SpecialArray []interface{}
+
+func ProductSum(array []interface{}) int {
+
+	return productSumHelper(array, 1)
+
+}
+func productSumHelper(array SpecialArray, d int) int {
+	var sum int
+	for _, elem := range array {
+		switch elem.(type) {
+		case SpecialArray:
+			sum += (d + 1) * productSumHelper(elem.(SpecialArray), d+1)
+		case int:
+			sum += elem.(int)
+		}
+	}
+	return sum
+}
--- recursion/recursion_test.go
+++ recursion/recursion_test.go
@@ -10,3 +10,9 @@ func TestGetNthFib(t *testing.T) {
 	//according to algoexpert GetNthFib(2) = F1
 	assert.Equal(t, 5, GetNthFib(6))
 }
+
+func TestProductSum(t *testing.T) {
+	var array SpecialArray
+	array = append(array, 5, 2, SpecialArray{7, -1}, 3, SpecialArray{6, SpecialArray{-13, 8}, 4})
+	assert.Equal(t, 12, ProductSum(array))
+}
