4ddad8301e3caa33e3b896beecd1b63a209ad2aa
shadai-rafael <shadai.lopez.garcia@gmail.com>
2021-03-21 20:11:05 -0600

    Insertion sort 1
    Signed-off-by: shadai-rafael <shadai.lopez.garcia@gmail.com>

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -2,12 +2,29 @@ package sort
 
 
 /**********************************************
-*	BubbleSort
+*	Bubble Sort
 ***********************************************/
 func TestBubbleSort(t *testing.T) {
 	array := int[]{3,2,1,0}
 	array = BubbleSort(array)
 	expected := int[]{0,1,2,3}
+	for i = range(array){
+		assert.Equal(t, array[i], expected[i]);
+	}
+}
+
+/**********************************************
+*	Insertion Sort
+***********************************************/
+func TestInsertionSort(t *testing.T) {
+	array := int[]{3,2,1,0}
+	array = InsertionSort(array)
+	expected := int[]{0,1,2,3}
+	for i = range(array){
+		assert.Equal(t, array[i], expected[i]);
+	}
+	array = int[]{7,4,2,6}
+	expected = int[]{2,4,7,6}
 	for i = range(array){
 		assert.Equal(t, array[i], expected[i]);
 	}
