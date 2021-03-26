7c96d54fb644738fe8083dde1d4b9cd5d6c8758e
shadai-rafael <shadai.lopez.garcia@gmail.com>
2021-03-25 22:28:40 -0600

    Merge sort solution 1
    Signed-off-by: shadai-rafael <shadai.lopez.garcia@gmail.com>

---
+++ sort/sortalg.go
@@ -0,0 +1,66 @@
+package sortalg
+
+/**********************************************
+*	Bubble Sort
+***********************************************/
+func BubbleSort(array []int) []int {
+	for i := range(array){
+		for j := 1; j < len(array)-i; j++ {
+			if array[j-1] > array[j]{
+				array[j-1], array[j] = array[j], array[j-1]
+			}
+		}
+	}
+	return array;
+}
+
+/**********************************************
+*	Insertion Sort
+***********************************************/
+func InsertionSort(array []int) []int {
+	for i := range(array){
+		for j := i; j > 0; j-- {
+			if array[j-1] > array[j]{
+				array[j-1], array[j] = array[j], array[j-1]
+			}else{
+				break
+			}
+		}
+	}
+	return array;
+}
+
+/**********************************************
+*	Merge Sort
+***********************************************/
+func MergeSort(array []int) []int {
+	if len(array) < 2 {
+		return array
+	}
+	lh := make([]int, len(array[:len(array)/2]))
+	copy(lh, array[:len(array)/2] )
+	hh := make([]int, len(array[len(array)/2:]))
+	copy(hh, array[len(array)/2:] )
+	lh = MergeSort(lh)
+	hh = MergeSort(hh)
+	i, j, k:= 0, 0, 0; 
+	for{
+		if i == len(lh) && j == len(hh){
+			break
+		}else if i == len(lh){
+			array[k] = hh[j]
+			j++
+		}else if j == len(hh){
+			array[k] = lh[i]
+			i++
+		}else if lh[i] > hh[j] {
+			array[k] = hh[j]
+			j++
+		}else{
+			array[k] = lh[i]
+			i++
+		}
+		k++
+	}
+	return array
+}
---
+++ sort/sortalg_test.go
@@ -0,0 +1,55 @@
+package sortalg
+
+import(
+//	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+/**********************************************
+*	Bubble Sort
+***********************************************/
+func TestBubbleSort(t *testing.T) {
+	array := []int{3,2,1,0}
+	array = BubbleSort(array)
+	expected := []int{0,1,2,3}
+	for i := range(array){
+		assert.Equal(t, array[i], expected[i]);
+	}
+}
+
+/**********************************************
+*	Insertion Sort
+***********************************************/
+func TestInsertionSort(t *testing.T) {
+	array := []int{3,2,1,0}
+	array = InsertionSort(array)
+	expected := []int{0,1,2,3}
+	for i := range(array){
+		assert.Equal(t, array[i], expected[i]);
+	}
+	array = []int{7,4,2,6}
+	array = InsertionSort(array)
+	expected = []int{2,4,6,7}
+	for i := range(array){
+		assert.Equal(t, array[i], expected[i]);
+	}
+}
+
+/**********************************************
+*	Merge Sort
+***********************************************/
+func TestMergeSort(t *testing.T) {
+	array := []int{3,2,1,0}
+	array = MergeSort(array)
+	expected := []int{0,1,2,3}
+	for i := range(array){
+		assert.Equal(t, array[i], expected[i]);
+	}
+	array = []int{7,4,2,6}
+	array = MergeSort(array)
+	expected = []int{2,4,6,7}
+	for i := range(array){
+		assert.Equal(t, array[i], expected[i]);
+	}
+}
