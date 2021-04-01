c6ab619d3480ad7a7054943bfb1265f4b1f8c9b0
shadai-rafael <shadai.lopez.garcia@gmail.com>
2021-04-01 11:46:59 -0600

    Find Three Largest Numbers solution 1
    Signed-off-by: shadai-rafael <shadai.lopez.garcia@gmail.com>

--- search/searchalg_test.go
+++ search/searchalg_test.go
@@ -16,3 +16,15 @@ func TestBinarySearch(t *testing.T) {
 	r = BinarySearch(array,100)
 	assert.Equal(t, r, -1);
 }
+
+/**********************************************
+*	Find Three Largest Numbers
+***********************************************/
+func TestFindThreeLargestNumbers(t *testing.T) {
+	array := []int{141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7}
+	result := FindThreeLargestNumbers(array)
+	expected := []int{18,141,541}
+	for i:= range(result){
+		assert.Equal(t, result[i], expected[i]);
+	}
+}
